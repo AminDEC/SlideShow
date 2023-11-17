27a08e64643729f5cdb24aff61f19e711ed87c05
Mostapha M Amin <mostaphamohammadiamin@gmail.com>
2023-11-17 19:40:00 +0330

    added the image view part

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ function App() {
   return (
     <div className="App">
       <img className="imag__back" src="2.png" alt="NO" />
+      <div className="image__container">
+        <img src="2.png" alt="" className="image__view" />
+        <div className="image__details">
+          <h1>image name</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            egestas nibh in varius tincidunt. In scelerisque elit et leo
+            interdum maximus. Nunc dapibus ante at augue maximus tincidunt.
+            Suspendisse tincidunt euismod hendrerit. Morbi ac turpis lacinia,
+            vehicula enim lacinia, porttitor urna. Nullam placerat risus eros,
+            vel convallis leo venenatis eu. Duis vel mattis massa. Fusce
+            venenatis leo est, ac gravida risus pretium in. Nam quis tortor
+            mauris. Donec ac enim vitae massa fermentum venenatis. Nam nec diam
+            condimentum, ornare ante eget, auctor odio.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
