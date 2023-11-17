3f57adba0129313a8c5fa2145b373db4343fcc52
Mostapha M Amin <mostaphamohammadiamin@gmail.com>
2023-11-17 20:18:17 +0330

    added the data files and some modifications to image view as well

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import "./App.css";
+import Pics from "./objects.js";
 
 function App() {
   return (
     <div className="App">
-      <img className="imag__back" src="2.png" alt="NO" />
-      <div className="image__container">
-        <img src="2.png" alt="" className="image__view" />
-        <div className="image__details">
-          <h1>image name</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            egestas nibh in varius tincidunt. In scelerisque elit et leo
-            interdum maximus. Nunc dapibus ante at augue maximus tincidunt.
-            Suspendisse tincidunt euismod hendrerit. Morbi ac turpis lacinia,
-            vehicula enim lacinia, porttitor urna. Nullam placerat risus eros,
-            vel convallis leo venenatis eu. Duis vel mattis massa. Fusce
-            venenatis leo est, ac gravida risus pretium in. Nam quis tortor
-            mauris. Donec ac enim vitae massa fermentum venenatis. Nam nec diam
-            condimentum, ornare ante eget, auctor odio.
-          </p>
+      <img className="imag__back" src={Pics[0].image_adrees} alt="NO" />
+      <div className="main__container">
+        <div className="image__container">
+          <img src={Pics[0].image_adrees} alt="" className="image__view" />
+          <div className="image__details">
+            <h1>{Pics[0].name}</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              egestas nibh in varius tincidunt. In scelerisque elit et leo
+              interdum maximus. Nunc dapibus ante at augue maximus tincidunt.
+              Suspendisse tincidunt euismod hendrerit. Morbi ac turpis lacinia,
+              vehicula enim lacinia, porttitor urna. Nullam placerat risus eros,
+              vel convallis leo venenatis eu. Duis vel mattis massa. Fusce
+              venenatis leo est, ac gravida risus pretium in. Nam quis tortor
+              mauris. Donec ac enim vitae massa fermentum venenatis. Nam nec
+              diam condimentum, ornare ante eget, auctor odio.
+            </p>
+          </div>
         </div>
+
+        <div className="slider__container"></div>
       </div>
     </div>
   );
---
+++ src/objects.js
@@ -0,0 +1,24 @@
+const Pics = [
+  {
+    name: "Italy City life",
+    image_adrees: "001.jpg",
+  },
+  {
+    name: "Italy Beach",
+    image_adrees: "002.jpg",
+  },
+  {
+    name: "Italy Nature",
+    image_adrees: "003.jpg",
+  },
+  {
+    name: "Italy Cities",
+    image_adrees: "004.jpg",
+  },
+  {
+    name: "Italt Farms",
+    image_adrees: "005.jpg",
+  },
+];
+
+export default Pics;
